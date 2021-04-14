418d49c9a24aae9aca09b9aff966127d757aaa47
Adriano Raiano <adriano@raiano.ch>
2021-04-14 11:30:02 +0200

    i18next setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+import { useState, Suspense } from 'react';
+import Footer from './Footer'
+
+const lngs = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' }
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const [count, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button key={lng} style={{ fontWeight: i18n.language === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {
+              i18n.changeLanguage(lng);
+              setCounter(count + 1);
+            }}>
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <i>{t('counter', { count })}</i>
+        </p>
+        <p>
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
@@ -15,11 +41,19 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('description.part2')}
         </a>
       </header>
+      <Footer t={t} />
     </div>
   );
 }
 
-export default App;
+// here app catches the suspense from page in case translations are not yet loaded
+export default function WrappedApp() {
+  return (
+    <Suspense fallback="...is loading">
+      <App />
+    </Suspense>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import i18n (needs to be bundled ;))
+import './i18n';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
