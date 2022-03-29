fdf7607eb04bab24b2130f736b04a07d6cd6bc2e
Adriano Raiano <adriano@raiano.ch>
2022-03-29 19:04:13 +0200

    update react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,11 +7,11 @@ import reportWebVitals from './reportWebVitals';
 // import i18n (needs to be bundled ;))
 import './i18n';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
